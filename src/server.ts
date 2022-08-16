c7f62cbfb86ca84f9364f64bc6b48549d03654bc
samuelcpr <samuelcpr464@gmail.com>
2022-08-16 10:08:12 -0300

    chore: integrated file server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from "express";
-import { createCourse } from "./routes";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
-app.get("/", createCourse);
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json({ foo: "sucesso total!" });
+});
 
-app.listen(3333);
+app.listen(3333, () => {
+	console.log("Aplicação executando na porta 3333!");
+});
