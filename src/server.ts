dc798c91f1ccd168c278c9b093ebece8c2e7545f
samuelcpr <samuelcpr464@gmail.com>
2022-08-17 10:20:41 -0300

    chore: update file

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
+import statusRoute from "routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(usersRoute);
+app.use(statusRoute);
 
 app.get("/users", (req: Request, res: Response, next: NextFunction) => {
 	res.status(200).send({ foo: "sucesso total!" });
